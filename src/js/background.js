fe32153a3cab1d4f20dcdff4e87c9d737ae710d8
Mark B <markallenblanchard@gmail.com>
2018-11-24 16:25:06 -0600

    put icons back

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,6 @@
 console.log('Lurking from the back, it is background.js!');
-
+import '../img/icon-128.png';
+import '../img/icon-34.png';
 import {wrapStore} from 'react-chrome-redux';
 import store from './store';
 
