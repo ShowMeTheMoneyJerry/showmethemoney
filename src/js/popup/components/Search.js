91696b5bbed0cd21ace049dcde789cd8250f4ae9
Esta Pitta <esta@Estas-MacBook-Air-2.local>
2018-11-27 21:44:05 -0600

    final designing touches

--- src/js/popup/components/Search.js
+++ src/js/popup/components/Search.js
@@ -1,107 +1,119 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import {withStyles} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
-import Snackbar from '@material-ui/core/Snackbar';
-import TextField from '@material-ui/core/TextField';
-import Icon from '@material-ui/core/Icon';
-import {connect} from 'react-redux';
-import {storeThunker} from '../../popup';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import Snackbar from "@material-ui/core/Snackbar";
+import TextField from "@material-ui/core/TextField";
+import Icon from "@material-ui/core/Icon";
+import { connect } from "react-redux";
+import { storeThunker } from "../../popup";
 
-import {fetchMostRecentPrice} from '../../store';
+import { fetchMostRecentPrice } from "../../store";
 
-const styles = (theme) => ({
-	appFrame: {
-		width: 360,
-		height: 360,
-		backgroundColor: 'white'
-	},
-	menuButton: {
-		marginLeft: -12,
-		marginRight: 20
-	},
-	button: {
-		margin: theme.spacing.unit
-		// backgroundColor: '#faf9f9'
-	},
-	textField: {
-		width: 200,
-		fullwidth: 'true',
-		transformOrigin: 'left'
-	},
-	rightIcon: {
-		marginLeft: theme.spacing.unit,
-		position: 'absolute'
-	}
+const styles = theme => ({
+  button: {
+    marginTop: 30,
+    marginBottom: 30,
+    marginLeft: 20,
+    marginRight: 10,
+    backgroundColor: "#a3dcac",
+    "&:hover": {
+      backgroundColor: "#128fa6",
+      color: "#FFFFFF"
+    }
+  },
+  textField: {
+    width: "100%",
+    transformOrigin: "left"
+  },
+  rightIcon: {
+    width: 30,
+    height: 20
+  },
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignSelf: "flex-end"
+  }
 });
 
 class Search extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			open: false,
-			name: ''
-		};
-		this.handleClick = this.handleClick.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-	}
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+      name: ""
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-	handleClick() {
-		this.setState({open: !this.state.open});
-	}
-	handleChange = (name) => (event) => {
-		this.setState({
-			[name]: event.target.value
-		});
-	};
+  handleClick() {
+    this.setState({ open: !this.state.open });
+  }
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
 
-	render() {
-		const {classes} = this.props;
-		const {open} = this.state;
-		return (
-			<div>
-				<TextField
-					id="standard-name"
-					label="Add Company"
-					className={classes.textField}
-					value={this.state.name}
-					onChange={this.handleChange('name')}
-					onSubmit={() => this.props.addCompany(this.state.name)}
-					margin="normal"
-				/>
-				<Button
-					variant="contained"
-					color="primary"
-					className={classes.button}
-					onClick={() => {
-						this.props.addCompany(this.state.name);
-						this.props.getMostRecentPrice(this.state.name);
-					}}
-				>
-					{/* <Icon className={classes.rightIcon} size="small">
-						add
-					</Icon> */}
-					<AddIcon className={classes.rightIcon} />
-				</Button>
-			</div>
-		);
-	}
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+    return (
+      <div className={classes.container}>
+        {open && (
+          <TextField
+            id="standard-name"
+            label="Add Company"
+            className={classes.textField}
+            value={this.state.name}
+            onChange={this.handleChange("name")}
+            onSubmit={() => this.props.addCompany(this.state.name)}
+            margin="normal"
+          />
+        )}
+        <Button
+          variant="contained"
+          className={classes.button}
+          onClick={() => {
+            if (this.state.open) {
+              if (this.state.name) {
+                this.props.addCompany(this.state.name);
+                this.props.getMostRecentPrice(this.state.name);
+              }
+
+              this.setState({ open: false, name: "" });
+            } else {
+              this.setState({ open: true });
+            }
+          }}
+        >
+          <AddIcon className={classes.rightIcon} />
+        </Button>
+      </div>
+    );
+  }
 }
 
-const mapState = (state) => ({
-	companies: state.companies
+const mapState = state => ({
+  companies: state.companies
 });
 
-const mapDispatch = (dispatch) => ({
-	getMostRecentPrice: (company) => dispatch(fetchMostRecentPrice(company))
+const mapDispatch = dispatch => ({
+  getMostRecentPrice: company => dispatch(fetchMostRecentPrice(company))
 });
 
-export default withStyles(styles)(connect(mapState, mapDispatch)(Search));
-//export default withStyles(styles)(Search);
+export default withStyles(styles)(
+  connect(
+    mapState,
+    mapDispatch
+  )(Search)
+);
